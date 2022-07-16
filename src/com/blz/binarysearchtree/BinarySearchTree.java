ffeb559857e749a83205b5740eb519d2a5f4d903
Him5696 <himanshi1252@gmail.com>
2022-07-16 21:42:37 +0530

    Added welcome messag

---
+++ src/com/blz/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.blz.binarysearchtree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary search Tree Problem");
+    }
+}
